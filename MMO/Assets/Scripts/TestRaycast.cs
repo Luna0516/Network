79b7246619d70dda5c3f19914036a32a3eb9c0fc
Luna0516 <130631559+Luna0516@users.noreply.github.com>
2024-07-24 15:20:28 +0900

    MMO_Unity
    Raycast 연습

---
+++ MMO/Assets/Scripts/TestRaycast.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRaycast : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 look = transform.forward;
+        Debug.DrawRay(transform.position + Vector3.up, look * 10, Color.red);
+
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(transform.position + Vector3.up, look, 10);
+
+        foreach (RaycastHit hit in hits)
+        {
+            Debug.Log($"Raycast {hit.collider.gameObject.name}");
+        }
+    }
+}
