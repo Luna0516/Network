a460c34be8975950736ca3ea4804e4fb647f3c36
Luna0516 <130631559+Luna0516@users.noreply.github.com>
2024-07-31 10:58:16 +0900

    MMO_Unity
    코루틴 실습

--- MMO/Assets/Scripts/Scenes/GameScene.cs
+++ MMO/Assets/Scripts/Scenes/GameScene.cs
@@ -4,27 +4,7 @@ using UnityEngine;
 
 public class GameScene : BaseScene
 {
-    class Test
-    {
-        public int Id = 0;
-    }
-
-    // Coroutine Test
-    // 1. 함수의 상태를 저장/복윈 가능!
-        // -> 엄청 오래 걸리는 작업을 잠시 끊거나
-        // -> 원하는 타이밍에 함수를 잠시 Stop/복원하는 경우
-    // 2. return -> 우리가 원하는 타입으로 가능(class도 가능)
-    class CoroutineTest : IEnumerable
-    {
-        public IEnumerator GetEnumerator()
-        {
-            for(int i = 0; i < 1000000; i++)
-            {
-                if(i % 10000 == 0)
-                    yield return null;
-            }
-        }
-    }
+    Coroutine co;
 
     protected override void Init()
     {
@@ -34,13 +14,14 @@ public class GameScene : BaseScene
 
         Managers.UI.ShowSceneUI<UI_Inven>();
 
+        co = StartCoroutine(ExplodeAfterSeconds(4.0f));
+    }
 
-        CoroutineTest test = new CoroutineTest();
-        foreach(var t in test)
-        {
-            Test value = (Test)t;
-            Debug.Log(value.Id);
-        }
+    IEnumerator ExplodeAfterSeconds(float second)
+    {
+        Debug.Log("Explode Enter");
+        yield return new WaitForSeconds(second);
+        Debug.Log("Explode Execute");
     }
 
     public override void Clear()
