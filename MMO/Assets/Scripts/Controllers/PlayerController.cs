4a2e9a4c188a334ba72855fd746bf2e0763e79a9
Luna0516 <130631559+Luna0516@users.noreply.github.com>
2024-08-01 18:27:54 +0900

    MMO_Unity
    CursorController로 커서 조정

--- MMO/Assets/Scripts/Controllers/PlayerController.cs
+++ MMO/Assets/Scripts/Controllers/PlayerController.cs
@@ -9,27 +9,12 @@ public class PlayerController : MonoBehaviour
 
     Vector3 _destPos;
 
-    Texture2D _attackIcon;
-    Texture2D _handIcon;
-
-    enum CursorType
-    {
-        None,
-        Attack,
-        Hand,
-    }
-
-    CursorType _cursorType = CursorType.None;
-
     void Start()
     {
         _stat = gameObject.GetOrAddComponent<PlayerStat>();
 
         Managers.Input.MouseAction -= OnMouseEvent;
         Managers.Input.MouseAction += OnMouseEvent;
-
-        _attackIcon = Managers.Resource.Load<Texture2D>("Textures/Cursors/Attack");
-        _handIcon = Managers.Resource.Load<Texture2D>("Textures/Cursors/Hand");
     }
 
     public enum PlayerState
@@ -103,38 +88,6 @@ public class PlayerController : MonoBehaviour
                 break;
             default:
                 break;
-        }
-
-        UpdateMouseCursor();
-    }
-
-    void UpdateMouseCursor()
-    {
-        if (Input.GetMouseButton(0))
-            return;
-
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
-
-        if (Physics.Raycast(ray, out RaycastHit hit, 100.0f, _mask))
-        {
-            if (hit.collider.gameObject.layer == (int)Define.Layer.Moster)
-            {
-                if (_cursorType != CursorType.Attack)
-                {
-                    Cursor.SetCursor(_attackIcon, new Vector2(_attackIcon.width / 5, 0), CursorMode.Auto);
-                    _cursorType = CursorType.Attack;
-                }
-            }
-            else
-            {
-                if (_cursorType != CursorType.Hand)
-                {
-                    Cursor.SetCursor(_handIcon, new Vector2(_handIcon.width / 3, 0), CursorMode.Auto);
-                    _cursorType = CursorType.Hand;
-                }
-            }
         }
     }
 
--- MMO/Assets/Scripts/Scenes/GameScene.cs
+++ MMO/Assets/Scripts/Scenes/GameScene.cs
@@ -11,6 +11,10 @@ public class GameScene : BaseScene
         SceneType = Define.Scene.Game;
 
         Managers.UI.ShowSceneUI<UI_Inven>();
+
+        Dictionary<int, Data.Stat> dict = Managers.Data.StatDict;
+
+        gameObject.GetOrAddComponent<CursorController>();
     }
 
     public override void Clear()
