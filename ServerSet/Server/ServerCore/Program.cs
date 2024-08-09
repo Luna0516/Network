6a9b584965cca198e6934c3645635d460aa8fc38
Luna0516 <130631559+Luna0516@users.noreply.github.com>
2024-08-09 15:09:28 +0900

    Server - SpinLock ===================

--- ServerSet/Server/ServerCore/Program.cs
+++ ServerSet/Server/ServerCore/Program.cs
@@ -4,92 +4,11 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
-    class FastLock
-    {
-        public int id;
-    }
-
-    // 데드락 테스트
-
-    class SessionManager
-    {
-        static object _lock = new object();
-
-        public static void TestSession()
-        {
-            lock (_lock)
-            {
-
-            }
-        }
-
-        public static void Test()
-        {
-            lock (_lock)
-            {
-                UserManager.TestUser();
-            }
-        }
-    }
-
-    class UserManager
-    {
-        static object _lock = new object();
-
-        public static void TestUser()
-        {
-            lock (_lock)
-            {
-
-            }
-        }
-
-        public static void Test()
-        {
-            lock (_lock)
-            {
-                SessionManager.TestSession();
-            }
-        }
-    }
-
     internal class Program
-    {
-        static int forSize = 10000;
-
-        static int number = 0;
-
-        static object lockObj = new object();
-
-        static void Thread_1()
-        {
-            for (int i = 0; i < forSize; i++)
-            {
-                SessionManager.Test();
-            }
-        }
-
-        static void Thread_2()
-        {
-            for (int i = 0; i < forSize; i++)
-            {
-                UserManager.Test();
-            }
-        }
-
+    { 
         static void Main(string[] args)
         {
-            Task t1 = new Task(Thread_1);
-            Task t2 = new Task(Thread_2);
-            t1.Start();
 
-            Thread.Sleep(100);
-
-            t2.Start();
-
-            Task.WaitAll(t1, t2);
-
-            Console.WriteLine(number);
         }
     }
 }
