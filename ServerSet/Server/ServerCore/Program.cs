0d738ca421459640604d3942cdf63d3c3789fea0
Luna0516 <130631559+Luna0516@users.noreply.github.com>
2024-08-11 23:01:40 +0900

    Server ------------------------------------

--- ServerSet/Server/ServerCore/Program.cs
+++ ServerSet/Server/ServerCore/Program.cs
@@ -6,44 +6,9 @@ namespace ServerCore
 {
     class Program
     {
-        static readonly int forSize = 100000;
-
-        static int _num = 0;
-
-        // 스핀락 보다 느리다 - 커널 동기화 객체라서
-        static Mutex _lock = new Mutex();
-
-        static void Thread_1()
-        {
-            for(int i = 0; i < forSize; i++)
-            {
-                _lock.WaitOne();
-                _num++;
-                _lock.ReleaseMutex();
-            }
-        }
-
-        static void Thread_2()
-        {
-            for (int i = 0; i < forSize; i++)
-            {
-                _lock.WaitOne();
-                _num--;
-                _lock.ReleaseMutex();
-            }
-        }
-
         static void Main(string[] args)
         {
-            Task t1 = new Task(Thread_1);
-            Task t2 = new Task(Thread_2);
 
-            t1.Start();
-            t2.Start();
-
-            Task.WaitAll(t1, t2);
-
-            Console.WriteLine(_num);
         }
     }
 }
