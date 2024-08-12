3588fc97f1cd5b9bd0257c1e0da8ea494479bb82
Luna0516 <130631559+Luna0516@users.noreply.github.com>
2024-08-12 10:30:09 +0900

    Server
    ReadWriteLock
    재귀적 락이 아닌 경우

--- ServerSet/Server/ServerCore/Program.cs
+++ ServerSet/Server/ServerCore/Program.cs
@@ -4,68 +4,11 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
-    // 1. 근성
-    // 2. 양보
-    // 3. 갑질
-
     class Program
     {
-        // 상호 배제
-        // Monitor
-        static object _lock = new object();
-        static SpinLock _spinLock = new SpinLock();
-        static Mutex _mutex = new Mutex(); // 사용X
-        // 직접 만든다
-
-        // [ ] [ ] [ ] [ ] [ ]
-        class Reward
-        {
-
-        }
-
-        // RWLock ReaderWriterLock
-        static ReaderWriterLockSlim _readerWriterLockSlim = new ReaderWriterLockSlim();
-
-        // 99.99999 - 0.00001
-        static Reward GetReward(int id)
-        {
-            _readerWriterLockSlim.EnterReadLock();
-            
-            _readerWriterLockSlim.ExitReadLock();
-
-            //lock (_lock)
-            //{
-                
-            //}
-
-            return null;
-        }
-
-        static void  AddReward(Reward reward)
-        {
-            lock(_lock)
-            {
-
-            }
-        }
-
         static void Main(string[] args)
         {
-            lock (_lock)
-            {
 
-            }
-
-            bool lockTaken = false;
-            try
-            {
-                _spinLock.Enter(ref lockTaken);
-            }
-            finally
-            {
-                if(lockTaken)
-                    _spinLock.Exit();
-            }
         }
     }
 }
--- ServerSet/Server/ServerCore/obj/Debug/net8.0/ServerCore.AssemblyInfo.cs
+++ ServerSet/Server/ServerCore/obj/Debug/net8.0/ServerCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb8fb179ee458e6ed804fe0fc6fc543e6dd3ec14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dd073bdae88a7a4a2b2ecbffc50d85c9c90f010")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
