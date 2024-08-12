75e830483e02869ebe3fa975b339d4ab512a48d3
Luna0516 <130631559+Luna0516@users.noreply.github.com>
2024-08-12 11:02:58 +0900

    Server
    TLS 실습

--- ServerSet/Server/ServerCore/Program.cs
+++ ServerSet/Server/ServerCore/Program.cs
@@ -6,9 +6,45 @@ namespace ServerCore
 {
     class Program
     {
+        // TLS
+        static ThreadLocal<string> ThreadName_TLS = new ThreadLocal<string>(() => { return $"My Name Is {Thread.CurrentThread.ManagedThreadId}"; });
+
+        // 
+        static string ThreadName_s = "";
+
+        static void WhoAmI_TLS()
+        {
+            bool repeat = ThreadName_TLS.IsValueCreated;
+
+            if (repeat)
+                Console.WriteLine(ThreadName_TLS.Value + " (repeat)");
+            else
+                Console.WriteLine(ThreadName_TLS.Value);
+        }
+
+        static void WhoAmI_s()
+        {
+            ThreadName_s = $"My Name Is {Thread.CurrentThread.ManagedThreadId}";
+
+            Thread.Sleep(1000);
+
+            Console.WriteLine(ThreadName_s);
+        }
+
         static void Main(string[] args)
         {
+            Parallel.Invoke(WhoAmI_s, WhoAmI_s, WhoAmI_s, WhoAmI_s, WhoAmI_s, WhoAmI_s);
 
+            Console.WriteLine("-----------------------------------------");
+
+            Thread.Sleep(1000);
+
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMaxThreads(3, 3);
+
+            Parallel.Invoke(WhoAmI_TLS, WhoAmI_TLS, WhoAmI_TLS, WhoAmI_TLS, WhoAmI_TLS, WhoAmI_TLS);
+
+            ThreadName_TLS.Dispose();
         }
     }
 }
--- ServerSet/Server/ServerCore/obj/Debug/net8.0/ServerCore.AssemblyInfo.cs
+++ ServerSet/Server/ServerCore/obj/Debug/net8.0/ServerCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3588fc97f1cd5b9bd0257c1e0da8ea494479bb82")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94cdb8fde22108279f8cdd8c1d91e4bf9d9a3b3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
