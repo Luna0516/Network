3f87458af9ee176ce95fa461240e794f50853e4d
Luna0516 <130631559+Luna0516@users.noreply.github.com>
2024-08-13 11:17:02 +0900

    Server --------------------------

--- ServerSet/Server/DummyClient/Program.cs
+++ ServerSet/Server/DummyClient/Program.cs
@@ -1,10 +1,10 @@
-﻿namespace DummyClient
+namespace DummyClient
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
         }
     }
 }
--- ServerSet/Server/ServerCore/obj/Debug/net8.0/ServerCore.AssemblyInfo.cs
+++ ServerSet/Server/ServerCore/obj/Debug/net8.0/ServerCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94cdb8fde22108279f8cdd8c1d91e4bf9d9a3b3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75e830483e02869ebe3fa975b339d4ab512a48d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
